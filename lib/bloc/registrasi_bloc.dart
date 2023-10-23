421f54dad8c1741b8185a029827621fe4a2e38bb
nuansarahardian <nuansa.rahardian@mhs.unsoed.ac.id>
2023-10-24 01:09:41 +0700

    tugas-kelima

---
+++ lib/bloc/registrasi_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:toko_api/helpers/api.dart';
+import 'package:toko_api/helpers/api_url.dart';
+import 'package:toko_api/model/registrasi.dart';
+
+class RegistrasiBloc {
+  static Future<Registrasi> registrasi(
+      {String? nama, String? email, String? password}) async {
+    String apiUrl = ApiUrl.registrasi;
+
+    var body = {"nama": nama, "email": email, "password": password};
+
+    var response = await Api().post(apiUrl, body);
+    var jsonObj = json.decode(response.body);
+    return Registrasi.fromJson(jsonObj);
+  }
+}
